8af4b8b7b5847641ae99d6ac87b0b96c6796426e
LALO18E <joedgoce@gmail.com>
2025-09-20 20:37:48 -0600

    Add files via upload

--- Recursividad_T4.py
+++ Recursividad_T4.py
@@ -13,24 +13,18 @@ def contar_digitos(n):
     return 1 + contar_digitos(n // 10)
 
 # 3. eliminar de una pila el valor en la posición media
-def eliminar_medio(pila):
+def eliminar_medio(pila, k=None):
     if not pila:
         return None
-    
-    n = len(pila)
-    mid = n // 2
-    list2 = []
-    
-    for i in range(mid):
-        list2.append(pila.pop())
-    
-    eliminado = pila.pop()
-    
-    while list2:
-        pila.append(list2.pop())
-    
-    return eliminado
+    if k is None:
+        k = len(pila) // 2
+    if k == 0:
+        return pila.pop()
+    tope = pila.pop()
+    eliminado = eliminar_medio(pila, k-1)
 
+    pila.append(tope)
+    return eliminado
 
 # 4. verificar si una cadena es palíndromo con recursividad
 def es_palindromo(cadena):
@@ -46,18 +40,21 @@ if __name__ == "__main__":
     # 1) Suma recursiva
     nums = [1, 2, 3, 4, 5]
     print("Suma recursiva:", suma_lista(nums))
+    print()
 
     # 2) Contar dígitos
     n = 12345
-    print("Número de dígitos en", n, ":", contar_digitos(n))  
+    print("Número de dígitos en", n, ":", contar_digitos(n))
+    print() 
 
     # 3) Eliminar elemento medio de pila
-    pila = [1, 2, 3, 4, 5]  # tope = 5
+    pila = [1, 2, 3, 4, 5]
     print("Pila original:", pila)
     eliminado = eliminar_medio(pila)
     print("Eliminado:", eliminado)
     print("Pila final:", pila)
+    print()
 
     # 4) Palíndromo
-    print("¿'radar' es palíndromo?:", es_palindromo("radar"))   # True
-    print("¿'python' es palíndromo?:", es_palindromo("python")) # False
+    print("¿La frase 'radar' es palíndromo?:", es_palindromo("radar"))
+    print("¿La frase 'python' es palíndromo?:", es_palindromo("python")) 
